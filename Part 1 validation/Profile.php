9a7b4f498d8b636556992bf7aa7772de398fdff5
FarisHollow <mirfaris79@gmail.com>
2022-06-25 00:56:34 +0600

    Step-1

---
+++ Part 1 validation/Profile.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+	<title>FORM</title>
+</head>
+<body align="center">
+        <h1><strong>PROFILE</strong></h1>
+		<form>
+			<fieldset>
+			  <legend>PROFILE</legend>
+				
+				<table>
+				
+					
+				
+					<tr>
+						<td>Edit Profile: </td>
+						<td>
+							<a href="editpro.php"><input type="button" name="" value="Click"></a>
+						</td>
+					</tr>
+					
+				
+					
+				</table>
+			</fieldset>
+		</form>
+
+</body>
+</html>
