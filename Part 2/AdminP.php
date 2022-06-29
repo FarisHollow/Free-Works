008c75912a2dbcd04be27a88ae276b8aecbe650a
FarisHollow <mirfaris79@gmail.com>
2022-06-30 01:44:40 +0600

    Updated

---
+++ Part 2/AdminP.php
@@ -0,0 +1,107 @@
+<?php 
+	session_start();
+	if(isset($_SESSION['status'])){
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin Panel</title>
+</head>
+<body>
+
+    
+	<!-- Start Header -->
+<table border="0" width="100%" cellpadding="3" cellspacing="3">
+	<tr>
+		 <td>
+			 <table border="0" width="85%" cellpadding="15%" cellspacing="0" align="center">
+			 <tr>
+					<td>
+						  
+						<h1><strong>Vivid</strong></h1>
+					  
+					</td>
+
+					<td width="60%">&nbsp;</td>
+
+					<td>
+           	   	  
+           	          <a href="Profile.php"> Profile </a>
+           	    
+           	        </td>
+			
+					<td>
+						
+					    <a href="Adminlogout.php"> Logout-></a>
+					
+					</td>
+			 </tr>
+			 </table>
+		 </td>
+	</tr>
+  </table>
+  <!-- End Header -->
+
+  <!-- Start Body -->
+ 
+	
+			 <table border="2" width="85%" cellpadding="15" cellspacing="0" align="center">
+			 <tr>
+					
+						 <h1 align="center">
+							
+							 Welcome back, Mr Admin 
+						
+						</h1>
+						 
+						<h2 align="center">To Admin panel</h2>
+					
+					
+						
+					   
+					
+			 </tr>
+		 
+		  <table border="2" width="30%" cellpadding="15" cellspacing="0" align="center">
+			<tr>
+			
+				<td>Manage Account: </td>
+			    
+				<td>
+					<a href="manage.php"><input type="Button" name="" value="Press"></a>
+				</td>
+			</tr>
+		  </table>
+		  <table border="2" width="20%" cellpadding="15" cellspacing="0" align="center">
+			<tr>
+			
+				<td>View Members:</td>
+			    
+				<td>
+					<input type="Button" name="" value="Press">
+				</td>
+			</tr>
+		  </table>
+		  <table border="1" width="10%" cellpadding="15" cellspacing="0" align="center">
+			<tr>
+			
+				<td>Users:</td>
+			    
+				<td>
+					<input type="Button" name="" value="Press">
+				</td>
+			</tr>
+		  </table>
+  
+   <!----End Body---->
+		
+
+</body>
+</html>
+
+<?php 
+	}else{
+		echo "invalid request";
+	}  
+?>
---
+++ Part 2/editpro.php
@@ -0,0 +1,42 @@
+
+
+
+<html>
+<head>
+	<title>Edit Profile</title>
+</head>
+<body align="center">
+	<h1><strong>EDIT PROFILE</strong></h1>
+	<form>
+			<fieldset>
+				<legend>EDIT</legend>
+				<table align="center">
+				<tr>
+		<td>Upload Photo: </td>
+		<td>
+			<input type="file" name="" value="">
+		</td>
+	</tr>
+	<tr>
+	    <td>Change Name:</td>
+		<td><input type="text" name="" value="" ></td>
+	</tr>
+	<tr>
+		<td>Add Description:</td>
+		<td>
+			<textarea></textarea>
+		</td>
+	</tr>
+					
+				
+				
+					
+					
+</table>
+</fieldset>
+</form>
+	
+</body>
+</html>
+
+
